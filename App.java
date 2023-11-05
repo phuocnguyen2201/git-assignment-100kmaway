a372255e364146fb77f1515780b7772e8f6b4d27
phuocnguyen2201 <phuocnguyen2201@gmail.com>
2023-11-05 07:04:29 +0000

    Initial code base

---
+++ App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner in = new Scanner(System.in);
+        
+/*      Car car1 = new Car();
+
+        car1.printData();
+        car1.accelerate();
+        car1.brake();
+        car1.printData(); */
+
+        System.out.println("Enter the amount of fuel to refuel: ");
+       // car1.refuel(in.nextInt());
+
+        System.out.println("---------------------------------");
+        System.out.println();
+
+       // Car car2 = new Car("Toyota", "Camry", 10);
+
+       // car2.printData();
+       // car2.accelerate();
+       // car2.brake();
+       // car2.printData();
+
+        System.out.println("Enter the amount of fuel to refuel: ");
+       // car2.refuel(in.nextInt());
+    }
+}
