0fd035df5b99a6d4caff1a6bd0c3568263fb30e7
Phuoc NG <phuocnguyen2201@gmail.com>
2023-11-05 09:11:44 +0200

    Making my commit to my branch

--- App.java
+++ App.java
@@ -5,27 +5,77 @@ public class App {
 
         Scanner in = new Scanner(System.in);
         
-/*      Car car1 = new Car();
+        Car car1 = new Car("BMW", "X5", 10);
 
-        car1.printData();
         car1.accelerate();
         car1.brake();
-        car1.printData(); */
+        car1.refuel(5);
+    }
+}
 
-        System.out.println("Enter the amount of fuel to refuel: ");
-       // car1.refuel(in.nextInt());
+class Car{
+    private String brand;
+    private String model;
+    private int amountOfFuel;
 
-        System.out.println("---------------------------------");
-        System.out.println();
+    public Car(){
+        brand = "";
+        model = "";
+        amountOfFuel = 100;
+        printData();
+    }
 
-       // Car car2 = new Car("Toyota", "Camry", 10);
+    public Car(String brand, String model, int amountOfFuel){
+        this.brand = brand;
+        this.model = model;
+        this.amountOfFuel = amountOfFuel;
+        printData();
+    }
 
-       // car2.printData();
-       // car2.accelerate();
-       // car2.brake();
-       // car2.printData();
+    public String getModel(){
+        return model;
+    }
 
-        System.out.println("Enter the amount of fuel to refuel: ");
-       // car2.refuel(in.nextInt());
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+
+    public int getFuel(){
+        return amountOfFuel;
+    }
+
+    public void setFuel(int amountOfFuel){
+        this.amountOfFuel = amountOfFuel;
+    }
+
+    void brake(){
+        System.out.println("Car is breaking");
+    }
+
+    void accelerate(){
+        amountOfFuel = amountOfFuel-- > 0 ? amountOfFuel : 0;
+        if(amountOfFuel > 0)
+            System.out.println("Car is accelerating");
+    }
+    private void printData(){
+        System.out.println("Brand: "+brand);
+        System.out.println("Model: "+model);
+        System.out.println("Fuel: "+amountOfFuel);
+    }
+
+    void refuel(int amountOfFuel){
+        System.out.println("Fuel in the tank: "+this.amountOfFuel);
+        System.out.println("Refuel: "+amountOfFuel);
+        this.amountOfFuel += amountOfFuel;
+
+        System.out.println("Fuel in the tank after the refuel: "+this.amountOfFuel);
     }
 }
